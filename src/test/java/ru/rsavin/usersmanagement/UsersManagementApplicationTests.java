2a662e1cd3f14087100ced9b3e4d5da90c53c1b3
Roman Savin <rsavin@aligntech.com>
2021-12-05 22:51:18 +0300

    failed test

--- src/test/java/ru/rsavin/usersmanagement/UsersManagementApplicationTests.java
+++ src/test/java/ru/rsavin/usersmanagement/UsersManagementApplicationTests.java
@@ -3,11 +3,18 @@ package ru.rsavin.usersmanagement;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 @SpringBootTest
 class UsersManagementApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void failedTest() {
+		fail();
 	}
 
 }
